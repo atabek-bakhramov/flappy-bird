ee84b1c0069943a75c51de92c12a792fa0395a24
atabek-bakh <coderonline@icloud.com>
2021-10-12 13:51:06 +0200

    game restarts on crossing borders

--- src/index.js
+++ src/index.js
@@ -1,25 +1,57 @@
 const Phaser = window.Phaser;
-
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
     default: "arcade",
+    arcade: {
+      gravity: { y: 400 },
+      debug: true,
+    },
   },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
 function preload() {
-  // 'this' context - scene
-  debugger;
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
 
+const VELOCITY = 200;
+
+let bird = null;
+const flapVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
+
 function create() {
-  debugger;
+  this.add.image(0, 0, "sky").setOrigin(0);
+  bird = this.physics.add
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, "bird")
+    .setOrigin(0);
+
+  this.input.on("pointerdown", flap);
+  this.input.keyboard.on("keydown_SPACE", flap);
+}
+
+function update() {
+  if (bird.y >= config.height || bird.y < 0) {
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
 }
 
 new Phaser.Game(config);
