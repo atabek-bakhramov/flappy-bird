14ca3f187f3304411673789da0fb7c0f0bcd05c4
atabek-bakh <coderonline@icloud.com>
2021-10-13 19:21:32 +0200

    infinite pipes are set

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Phaser = window.Phaser;
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -17,14 +18,15 @@ const config = {
 };
 
 const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 
 let bird = null;
+let pipes = null;
 
-let upperPipe = null;
-let lowerPipe = null;
+let pipeHorizontalDistance = 0;
 
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+const pipeHorizontalDistanceRange = [500, 550];
 
 const flapVelocity = 250;
 const initalBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
@@ -42,21 +44,69 @@ function create() {
     .setOrigin(0);
   bird.body.gravity.y = 400;
 
-  upperPipe = this.physics.add.sprite(400, 100, "pipe").setOrigin(0, 1);
-  lowerPipe = this.physics.add
-    .sprite(400, upperPipe.y + pipeVerticalDistance, "pipe")
-    .setOrigin(0, 0);
+  pipes = this.physics.add.group();
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
+  }
+
+  pipes.setVelocityX(-200);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-// if bird y position is small than 0 or greater than height of the canvas
-// then alert "you have lost"
 function update() {
   if (bird.y > config.height || bird.y < -bird.height) {
     restartBirdPosition();
   }
+
+  recyclePipes();
+}
+
+function placePipe(uPipe, lPipe) {
+  const rightMostX = getRightMostPipe();
+  const pipeVerticalDistance = Phaser.Math.Between(
+    ...pipeVerticalDistanceRange
+  );
+  const pipeVerticalPosition = Phaser.Math.Between(
+    0 + 20,
+    config.height - 20 - pipeVerticalDistance
+  );
+  const pipeHorizontalDistance = Phaser.Math.Between(
+    ...pipeHorizontalDistanceRange
+  );
+
+  uPipe.x = rightMostX + pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+function recyclePipes() {
+  const tempPipes = [];
+  pipes.getChildren().forEach((pipe) => {
+    if (pipe.getBounds().right <= 0) {
+      tempPipes.push(pipe);
+      if (tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  });
+}
+
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function (pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+
+  return rightMostX;
 }
 
 function restartBirdPosition() {
